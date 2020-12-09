a6e609f2350ab2a3570a4a0b00aeec6c14da1f7e
zwx746910 <857314548@qq.com>
2020-12-09 21:12:41 +0800

    项目1209提交

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -15,6 +18,9 @@ public class PaymentController {
 
     @Resource
     private PaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @Value("${server.port}")
     private String serverPort;
@@ -39,5 +45,20 @@ public class PaymentController {
         } else {
             return new CommonResult(200,"success,serverPort:"+serverPort,payment);
         }
+    }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object getDiscovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("*******payment:"+service);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info("*****instance:"+instance.getInstanceId()+"\t"+instance.getHost()+"\t"+instance.getUri());
+        }
+
+        return this.discoveryClient;
     }
 }
